00c74a7bca2b980547b2a913cc6a79050e8064f1
RogerXPZhang <roger.xp.zhang@gmail.com>
2016-12-09 11:22:54 -0500

    Add files via upload
    Code for inner and outer LIM

--- MotorController.ino
+++ MotorController.ino
@@ -2,6 +2,15 @@ unsigned int nHits = 0;
 float rotationRate = 0.0f;
 char flip;
 char input;
+unsigned char b_direction=0;
+unsigned char b_commandTarget=0;
+
+#define BOTHACTUATOR     0b00000011
+#define INNERACTUATOR    0b00000001
+#define OUTERACTUATOR    0b00000010
+
+
+
 void setup()
 {
   pinMode(23,INPUT); //INDEX
@@ -11,11 +20,10 @@ void setup()
   Serial.setTimeout(200);
   Serial.println("Wellcome!");
   Serial.print('>');
-  
+
   init_SinWAVE();
-  setAmplitudeSinWAVE(0.25);
-  setFrequencySinWAVE(100);
-  //startSinWAVE();
+  setAmplitudeSinWAVE(0.25,BOTHACTUATOR);
+  setFrequencySinWAVE(100,BOTHACTUATOR);
 
 
 }
@@ -26,12 +34,12 @@ void encoder()
 }
 void loop()
 {
-  
-   fire100();
+
+  fire100();
   //delay(1000);
   //Serial.println(nHits / 2500.0f);
   //nHits = 0;
-  
+
 }
 
 
@@ -42,8 +50,34 @@ void fire100()
     char input = Serial.read();
     Serial.println(input);
     switch(input){
-      
-      case 'a': //change amplitude
+
+    case 'i':
+      {
+        b_commandTarget = INNERACTUATOR;
+        break;
+      }
+    case 'o':
+      {
+        b_commandTarget = OUTERACTUATOR;
+        break;
+      }
+    case 'b':
+      {
+        b_commandTarget = BOTHACTUATOR;
+        break;
+      }
+    default:
+      Serial.println("Please start the command with i or e");
+      return;
+      break;
+    }
+  }
+  
+  if(Serial.available()){
+    char input = Serial.read();
+    Serial.println(input);
+    switch(input){
+    case 'a': //change amplitude
       {
         Serial.println("enter new amp (0to1):");
         Serial.flush();
@@ -53,17 +87,17 @@ void fire100()
           Serial.println("Amp out of range.");
           break;
         }
-        setAmplitudeSinWAVE(newAmp);
+        setAmplitudeSinWAVE(newAmp,b_commandTarget);
         Serial.print("Amp set to ");
         Serial.print(newAmp * 100);
         Serial.println("%");
       }
       break;
 
-      case 'w': 
+    case 'w': 
 
       for(int i = 0; i < 256; ++i){
-        Serial.print(sinon[i]);
+        //Serial.print(sinon[i]);
         Serial.print(' ');
       }
 
@@ -71,20 +105,20 @@ void fire100()
 
       break;
 
-      case 'f': //change frequency
+    case 'f': //change frequency
       {
         Serial.println("enter new frequency:");
         Serial.flush();
         while(!Serial.available());
         float freq = Serial.parseFloat();
-        setFrequencySinWAVE(freq);
+        setFrequencySinWAVE(freq,b_commandTarget);
         Serial.print("frequency set to ");
         Serial.print(freq);
         Serial.println("Hz");
       }
       break;
 
-      case 't': //encoder values
+    case 't': //encoder values
       {
         Serial.println("waiting for encoder");
         Serial.flush();
@@ -92,118 +126,128 @@ void fire100()
         Serial.println("found encoder");
         int count=0;
         long t1=millis();
-        startSinWAVE();
+        startSinWAVE(b_commandTarget);
         while(count<431)
         {
           while(digitalRead(23));
           count+=1;
           while(!digitalRead(23));
-        
+
         }
-        stopSinWAVE();
+        stopSinWAVE(b_commandTarget);
         digitalWrite(2,LOW);
         digitalWrite(3,LOW);
         digitalWrite(5,LOW);
-        
+
         t1=millis()-t1;
-        
+
         Serial.print(count);
         Serial.print(" encoder pulses in ");
         Serial.println(t1);
-        
-        
+
+
       }
       Serial.println("done");
       break;
 
-      case 'n':
-        //get time
-        Serial.println("enter Time (ms)");
-        {
-          Serial.flush();
-          while(!Serial.available());
-          int runtime = Serial.parseInt();
-          Serial.print("Running for ");
-          Serial.print(runtime);
-          Serial.println("ms");
-
-          
-          //pulse
-          startSinWAVE();
-          delay(runtime);
-          stopSinWAVE();
-          digitalWrite(2,LOW);
-          digitalWrite(3,LOW);
-          digitalWrite(5,LOW);
-        }
-        Serial.println("done");
-        break;
-
-        
-      case 'z':
-        //get time
-        Serial.println("enter Time (ms)");
-        {
-          Serial.flush();
-          while(!Serial.available());
-          int runtime = Serial.parseInt();
-          Serial.print("Running for ");
-          Serial.print(runtime);
-          Serial.println("ms");
-
-          
-          //pulse
-          //wait for index pulse
-          Serial.println("waiting");
-          while(!digitalRead(22));
-          Serial.println("found");
-          startSinWAVE();
-          delay(runtime);
-          stopSinWAVE();
-          digitalWrite(2,LOW);
-          digitalWrite(3,LOW);
-          digitalWrite(5,LOW);
-        }
-        Serial.println("done");
-        break;
-        
-        case 'm':
-        //get time
-        {
-          Serial.flush();
-          Serial.println("enterAtocontinue");
-          
-          //pulse
-          startSinWAVE();
-          
-          while(!Serial.available());
-          
-          stopSinWAVE();
-          flip=Serial.read();
-          Serial.flush();
-          
-          digitalWrite(2,LOW);
-          digitalWrite(3,LOW);
-          digitalWrite(5,LOW);
-        }
-        
-        Serial.println("done");
-        break;
-
-        case 'p': //encoder values
+    case 'n':
+      //get time
+      Serial.println("enter Time (ms)");
       {
-        plug();
-        Serial.println("direction swapped");
-        
+        Serial.flush();
+        while(!Serial.available());
+        int runtime = Serial.parseInt();
+        Serial.print("Running for ");
+        Serial.print(runtime);
+        Serial.println("ms");
+
+
+        //pulse
+        startSinWAVE(b_commandTarget);
+        delay(runtime);
+        stopSinWAVE(b_commandTarget);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(5,LOW);
       }
       Serial.println("done");
       break;
-  
-      default:
+
+
+    case 'z':
+      //get time
+      Serial.println("enter Time (ms)");
+      {
+        Serial.flush();
+        while(!Serial.available());
+        int runtime = Serial.parseInt();
+        Serial.print("Running for ");
+        Serial.print(runtime);
+        Serial.println("ms");
+
+
+        //pulse
+        //wait for index pulse
+        Serial.println("waiting");
+        while(!digitalRead(22));
+        Serial.println("found");
+        startSinWAVE(b_commandTarget);
+        delay(runtime);
+        stopSinWAVE(b_commandTarget);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(5,LOW);
+      }
+      Serial.println("done");
+      break;
+
+    case 'm':
+      //get time
+      {
+        Serial.flush();
+        Serial.println("enterAtocontinue");
+
+        //pulse
+        startSinWAVE(b_commandTarget);
+
+        while(!Serial.available());
+
+        stopSinWAVE(b_commandTarget);
+        flip=Serial.read();
+        Serial.flush();
+
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(5,LOW);
+      }
+
+      Serial.println("done");
+      break;
+
+    case 'p': //encoder values
+      {
+        plug(b_commandTarget);
+        Serial.print("direction swapped to ");
+        if(b_direction){
+          Serial.println("forward");
+        }
+        else{
+          Serial.println("backward");
+        }
+        b_direction = !b_direction;
+
+      }
+      Serial.println("done");
+      break;
+
+    default:
       Serial.println("?");
     }
     Serial.print('>');
   }
 
-  
+
 }
+
+
+
